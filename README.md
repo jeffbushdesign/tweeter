16b450732045f4e0a85e1feb672c0d509a44add6
Jeff Bush <jeffbushdesign@gmail.com>
2022-03-26 14:42:08 +0000

    Prevent cross-site scripting XSS using an escape function

--- README.md
+++ README.md
@@ -9,10 +9,35 @@ This repository is the starter code for the project: Students will fork and clon
 1. [Create](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template) a new repository using this repository as a template.
 2. Clone your repository onto your local device.
 3. Install dependencies using the `npm install` command.
-3. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
-4. Go to <http://localhost:8080/> in your browser.
+4. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
+5. Go to <http://localhost:8080/> in your browser.
 
 ## Dependencies
 
 - Express
 - Node 5.10.x or above
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script>
+$(function() {
+  const $button = $('#load-more-posts');
+  $button.on('click', function () {
+    console.log('Button clicked, performing ajax call...');
+    $.ajax('more-posts.html', { method: 'GET' })
+    .then(function (morePostsHtml) {
+      console.log('Success: ', morePostsHtml);
+      $button.replaceWith(morePostsHtml);
+    });
+  });
+});
+</script>
+
+$.ajax({
+url: "https://app.asana.com/-/api/0.1/workspaces/",
+type: 'GET',
+dataType: 'json', // added data type
+success: function(res) {
+console.log(res);
+alert(res);
+}
+});
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,12 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const tweetData = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
+//function prevents XSS with escaping
+const escape = function (str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 const createTweetElement = function (tweetObj) {
   console.log('Tweet Object', tweetObj);
@@ -39,7 +20,7 @@ const createTweetElement = function (tweetObj) {
           <span id="name">${tweetObj.user.name}</span>
           <span id="handle">${tweetObj.user.handle}</span>
         </header>
-        <span class="tweet">${tweetObj.content.text}</span>
+        <span class="tweet">${escape(tweetObj.content.text)}</span>
         <footer>
           <span class="days-ago">${timeago.format(tweetObj.created_at)}</span>
   <div id="icons">
